16d3c940ac9e25199f65ed5de268bc9899f612f3
Ian H. Kessler <ianholm9@gmail.com>
2022-10-25 19:48:23 -0600

    Created some numpy functions.

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ from preprocessing import Preprocessing
 import pandas as pd
 from DataDictionary import DataDictionary
 import os
+import IF1
+import numpy as np
 
 def main_Ian():
     DD = DataDictionary()
     data = DD.dataobject(True, "Abalone")
-    print(data.df.head(10))
-    dataobjects = DD.dataobjects(True)
-    print(dataobjects)
-    for name in DD.datanames:
-        print(dataobjects[name].df.head(10))
+    v = range(10)
+    m = np.matrix([v, v, v])
+    x = IF1.value(data)(0)
+    print(x)
+    print(IF1.sigmoid_v(x))
+    print(IF1.matrixmultiply(m)(x))
 
 
 
