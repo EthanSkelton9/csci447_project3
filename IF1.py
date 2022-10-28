da76fd0c39bed6641198307c47be2779e7568595
CategorIAN <ianholm9@gmail.com>
2022-10-28 09:42:33 -0600

    Created predict and error function.

--- IF1.py
+++ IF1.py
@@ -2,18 +2,37 @@ import pandas as pd
 import math
 import numpy as np
 
-def value(data):
+def colvec(data):
     def f(i):
         return np.c_[data.df.loc[i, data.features_ohe]]
     return f
 
 def matrixmultiply(matrix):
     def f(vector):
-        return matrix * vector
+        return matrix @ vector
     return f
 
 def sigmoid(x):
-    return 1 / (1 + math.exp(x))
+    return 1 / (1 + math.exp(-x))
 
 def sigmoid_v(x):
     return np.vectorize(sigmoid)(x)
+
+def rand_w(n, m):
+    return (.01 - (-.01)) * np.random.random_sample((n, m)) - .01
+
+def predict_value(data):
+    w = rand_w(1, data.df.shape[1] - 1)
+    c = colvec(data)
+    def f(i):
+        return (w @ c(i))[0, 0]
+    return f
+
+def error(data):
+    pred_func = predict_value(data)
+    actual_func = lambda i: data.df.at[i, "Target"]
+    def f(i):
+        predicted = pred_func(i)
+        actual = actual_func(i)
+        return .5 * math.pow(actual - predicted, 2)
+    return f
--- main.py
+++ main.py
@@ -6,14 +6,20 @@ import IF1
 import numpy as np
 
 def main_Ian():
-    DD = DataDictionary()
-    data = DD.dataobject(True, "Abalone")
-    v = range(10)
-    m = np.matrix([v, v, v])
-    x = IF1.value(data)(0)
-    print(x)
-    print(IF1.sigmoid_v(x))
-    print(IF1.matrixmultiply(m)(x))
+    def f1():
+        DD = DataDictionary()
+        data = DD.dataobject(True, "Abalone")
+        v = range(10)
+        m = np.matrix([v, v, v])
+        x = IF1.value(data)(0)
+        print(x)
+        print(IF1.sigmoid_v(x))
+        print(IF1.matrixmultiply(m)(x))
+    def f2():
+        DD = DataDictionary()
+        data = DD.dataobject(True, "Abalone")
+        print(IF1.error(data)(0))
+    return f2()
 
 
 
