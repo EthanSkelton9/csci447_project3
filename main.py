aeae6b2a07cd2304d8788c8482e63d2a0a24ccd7
Ethan <bigeskelly@gmail.com>
2022-11-06 18:40:59 -0700

    pulling

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ from CrossValidation import CrossValidation as CV
 def main_Ian():
     def f1():
         DD = DataDictionary()
-        data = DD.dataobject(True, "SoyBean")
+        data = DD.dataobject(True, "Abalone")
         NN = Neural_Net(data)
-        y = NN.stochastic_online_gd()(eta=0.1, max_error=1, hidden_vector = [8, 4], alpha = 0.9)
+        # y = NN.stochastic_online_gd()(eta=0.1, max_error=1, hidden_vector = [8, 4], alpha = 0.9)
+        y = NN.tuning([1], False)
+        print(y)
     def f2():
         DD = DataDictionary()
         data = DD.dataobject(True, "SoyBean")
         DataCV = CV(data)
         print(DataCV.training_test_dicts(data.df))
-    return f2()
+    return f1()
 
 
 
@@ -33,4 +35,4 @@ def main():
 if __name__=="__main__":
     main_Ian()
     #main()
-    #y = NN.tuning([3,1,1])
+    
