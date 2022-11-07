0e27a0ce4f5ab56d8e8067e72342a6ad26a32fe3
Ethan <bigeskelly@gmail.com>
2022-11-07 13:04:44 -0700

    fixed analysis

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ def main_Ian():
         print(y)
     def f2():
         DD = DataDictionary()
-        data = DD.dataobject(True, "Abalone")
+        data = DD.dataobject(True, "Hardware")
         DataCV = CV(data)
-        DataCV.test(eta_space = np.linspace(0.1, 0.3, 3), alpha_space = [0, 0.8, 0.9], new=False, appendCount=3)
+        # DataCV.test(eta_space = np.linspace(0.1, 0.3, 3), alpha_space = [0, 0.8, 0.9], new=False, appendCount=3)
+        DataCV.analysisFunction()
     return f2()
 
 
